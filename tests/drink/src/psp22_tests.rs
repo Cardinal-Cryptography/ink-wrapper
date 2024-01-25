7bdbffe2b0db488a7c6e3aa6bc6a48478183a798
deuszx <mateusz.gorski@cardinals.cc>
2024-01-25 18:08:00 +0100

    RM unnecessary code

--- tests/drink/src/psp22_tests.rs
+++ tests/drink/src/psp22_tests.rs
@@ -10,10 +10,10 @@ use crate::*;
 pub fn balance_of(
     session: &mut Session<MinimalRuntime>,
     psp22: Instance,
-    account: AccountId,
+    account: impl Into<AccountId>,
 ) -> u128 {
     session
-        .query(psp22.balance_of(account))
+        .query(psp22.balance_of(account.into()))
         .unwrap()
         .result
         .unwrap()
@@ -34,26 +34,26 @@ pub fn setup(session: &mut Session<MinimalRuntime>, caller: drink::AccountId32) 
 
 #[test]
 fn test_transfers() -> Result<()> {
-    let mut session: Session<MinimalRuntime> = Session::new().expect("Init new Session");
+    let mut session = Session::new().expect("Init new Session");
     let instance = setup(&mut session, BOB);
 
     let transfer_amount = 100;
 
-    let alice_balance = balance_of(&mut session, instance, alice().into());
-    let bob_balance = balance_of(&mut session, instance, bob().into());
+    let alice_balance = balance_of(&mut session, instance, alice());
+    let bob_balance = balance_of(&mut session, instance, bob());
 
     let _res = session
         .execute(instance.transfer(alice().into(), transfer_amount, vec![]))
         .unwrap();
 
-    assert!(balance_of(&mut session, instance, bob().into()) == bob_balance - transfer_amount);
-    assert!(balance_of(&mut session, instance, alice().into()) == alice_balance + transfer_amount);
+    assert!(balance_of(&mut session, instance, bob()) == bob_balance - transfer_amount);
+    assert!(balance_of(&mut session, instance, alice()) == alice_balance + transfer_amount);
     Ok(())
 }
 
 #[test]
 fn test_burn() -> Result<()> {
-    let mut session: Session<MinimalRuntime> = Session::new().expect("Init new Session");
+    let mut session = Session::new().expect("Init new Session");
     let instance = setup(&mut session, BOB);
     let supply_before = session
         .query(instance.total_supply())
